e5747aebf4422a6e0a71148719aace67520000df
aurorasarkka <77781578+aurorasarkka@users.noreply.github.com>
2022-09-16 22:45:57 +0300

    saatu pelistä kuva näkymään

--- app/src/main/java/com/example/android/GameActivity.java
+++ app/src/main/java/com/example/android/GameActivity.java
@@ -1,5 +1,6 @@
 package com.example.android;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -69,9 +70,9 @@ public class GameActivity extends AppCompatActivity {
             }
         });
 
-
+        //random number generator
         Random rand = new Random();
-        rand_int1 = rand.nextInt(4);
+        rand_int1 = rand.nextInt(3+1);
 
         Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation1);
         ImageView image = (ImageView) findViewById(R.id.imagebutton1);
@@ -79,24 +80,50 @@ public class GameActivity extends AppCompatActivity {
 
     }
 
-
+    //jokainen kortti -> voi tulla tyhjiä ei löyä joka kerta
     public void handleClickEvents(View view) {
         switch (view.getId()) {
             case R.id.imagebutton1:
                 gamebutton1.setVisibility(View.INVISIBLE);
                 gamebutton1.startAnimation((animation));
+                if(rand_int1 == 1) {
+                    gamebutton1.setImageResource(R.mipmap.music_note);
+                    gamebutton1.setBackgroundColor(Color.GREEN);
+                    gamebutton1.setVisibility(View.VISIBLE);
+
+                }
+
                 break;
             case R.id.imagebutton2:
                 gamebutton2.setVisibility(View.INVISIBLE);
                 gamebutton2.startAnimation((animation));
+                if(rand_int1 == 2) {
+                    gamebutton2.setImageResource(R.mipmap.music_note);
+                    gamebutton2.setBackgroundColor(Color.GREEN);
+                    gamebutton2.setVisibility(View.VISIBLE);
+
+                }
                 break;
             case R.id.imagebutton3:
                 gamebutton3.setVisibility(View.INVISIBLE);
                 gamebutton3.startAnimation((animation));
+                if(rand_int1 == 3) {
+                    gamebutton3.setImageResource(R.mipmap.music_note);
+                    gamebutton3.setBackgroundColor(Color.GREEN);
+                    gamebutton3.setVisibility(View.VISIBLE);
+
+                }
+
                 break;
             case R.id.imagebutton4:
                 gamebutton4.setVisibility(View.INVISIBLE);
                 gamebutton4.startAnimation((animation));
+                if(rand_int1 == 4) {
+                    gamebutton4.setImageResource(R.mipmap.music_note);
+                    gamebutton4.setBackgroundColor(Color.GREEN);
+                    gamebutton4.setVisibility(View.VISIBLE);
+
+                }
                 break;
 
             case R.id.fab:
